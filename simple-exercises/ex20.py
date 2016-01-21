5330572b8099bb201b076849f617d86929a1fae3
Katya Demidova <demidovakatya@gmail.com>
2016-01-21 15:30:30 +0300

    More problems

---
+++ simple-exercises/ex20.py
@@ -0,0 +1,22 @@
+'''
+Represent a small bilingual lexicon as a Python dictionary in 
+the following fashion
+{"merry":"god", "christmas":"jul", "and":"och", "happy":gott",
+"new":"nytt", "year":"år"} 
+and use it to translate your Christmas cards from English into Swedish. 
+
+That is, write a function translate() that takes a list of English words and 
+returns a list of Swedish words.
+'''
+
+dictionary = {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", 
+                                "new":"nytt", "year":"år"} 
+
+def translate(english):
+    swedish = []
+    for word in english:
+        swedish += [dictionary[word.lower()]]
+    return swedish
+
+#test
+print(translate(['Merry', 'christmas', 'and', 'happy', 'new', 'year']))
