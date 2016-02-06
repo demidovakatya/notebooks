49373b9010c58f45d9f86015b0fdb20dbaa5be42
Katya Demidova <demidovakatya@gmail.com>
2016-02-06 19:17:35 +0300

    Configs for music game

---
+++ music_game/config.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+token = '110831855:AAE_GbIeVAUwk11O12vq4UeMnl20iADUtM'  # Токен бота - invalid
+database_name = 'music.db'  # Файл с базой данных
+shelve_name = 'shelve.db'  # Файл с хранилищем
