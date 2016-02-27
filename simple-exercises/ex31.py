6f48e5aded12c55fef35f7bac2dbb9631ff69a9d
Katya Demidova <demidovakatya@gmail.com>
2016-02-27 09:05:49 +0300

    Add ex45

--- simple-exercises/ex31.py
+++ simple-exercises/ex31.py
@@ -1,4 +1,5 @@
-# Implement the higher order functions map(), filter() and reduce(). (They are built-in but writing them yourself may be a good exercise.)
+# Implement the higher order functions map(), filter() and reduce(). 
+# (They are built-in but writing them yourself may be a good exercise.)
 
 # map(func, *iterables) --> map object
 def map(func, iterable):
@@ -6,8 +7,17 @@ def map(func, iterable):
 
 # filter(function or None, iterable) --> filter object
 def filter(func, iterable):
-    pass
+    return [item for item in iterable if func(item)]
 
 # 
-def reduce():
-    pass
+def reduce(func, iterable):
+    if len(iterable) == 2:
+        return func(iterable[0], iterable[-1])
+    else:
+        return func(reduce(func, iterable[0:-1]), iterable[-1])
+
+# test
+numbers = [1, 2, 3, 4, 5]
+print(map(lambda x: x * 2, numbers))
+print(filter(lambda x: x > 3, numbers))
+print(reduce(lambda x, y: x + y, numbers))
